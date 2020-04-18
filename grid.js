d6d0ed8467bf2d7671ad9cd528f1f04ef4869baf
Gary Bishop <gary.bishop.unc@gmail.com>
2020-04-17 20:12:42 -0400

    update

--- grid.js
+++ grid.js
@@ -1,4 +1,5 @@
 import Reef from "./reef.js";
+
 Reef.debug(true);
 
 const data = {
@@ -16,34 +17,51 @@ const data = {
     "help",
     "he",
     "here",
-    "in"
+    "in",
   ],
-  selected: 0
+  selected: -1,
 };
 
-/** @typedef {typeof data} Props */
+/**
+ * @typedef {typeof data} Props
+ */
+
+/** tag html template strings so prettier will format them and vim will highlight them
+ * @returns string
+ * @param {TemplateStringsArray} strings
+ * @param {any[]} values
+ */
+function html(strings, ...values) {
+  let str = "";
+  strings.forEach((s, i) => {
+    str += s + (values[i] || "");
+  });
+  return str;
+}
 
 const app = new Reef(".page", {
   data: data,
   /** @param {Props} props */
-  template: props => {
+  template: (props) => {
     document.body.style.setProperty("--nrows", "" + props.height);
     document.body.style.setProperty("--ncols", "" + props.width);
-    var html = "";
+    let result = "";
     props.words.forEach((word, index) => {
-      html += `<div class="symbol">
-        <figure class="${props.selected === index ? "selected" : ""}">
-          <img src="images/${word}.png" />
-          <figcaption>${word}</figcaption>
-        </figure>
-      </div>`;
+      result += html`
+        <div class="symbol">
+          <figure class="${props.selected === index ? "selected" : ""}">
+            <img src="images/${word}.png" />
+            <figcaption>${word}</figcaption>
+          </figure>
+        </div>
+      `;
     });
-    return html;
-  }
+    return result;
+  },
 });
 
 window.addEventListener("load", () => app.render());
-document.addEventListener("keydown", e => {
+document.addEventListener("keydown", (e) => {
   console.log("kd", e);
   const data = app.getData();
   if (["ArrowRight", "Space", "Tab"].includes(e.code)) {
@@ -55,7 +73,71 @@ document.addEventListener("keydown", e => {
     speechSynthesis.speak(msg);
   }
 });
-document.addEventListener("render", e => {
+
+var tx = -1,
+  ty = -1,
+  tt = -1;
+
+document.addEventListener("touchstart", startHandler);
+document.addEventListener("mousedown", startHandler);
+
+/** @param {Event} e */
+function startHandler(e) {
+  console.log("ts", e);
+  e.preventDefault();
+  if (e instanceof TouchEvent) {
+    const ct = e.changedTouches[0];
+    tx = ct.screenX;
+    ty = ct.screenY;
+    tt = e.timeStamp;
+  } else if (e instanceof MouseEvent) {
+    tx = e.screenX;
+    ty = e.screenY;
+    tt = e.timeStamp;
+  }
+}
+
+document.addEventListener("touchend", endHandler);
+document.addEventListener("mouseup", endHandler);
+
+/** @param {Event} e */
+function endHandler(e) {
+  console.log("te", e);
+  const data = app.getData();
+  const dx =
+    (e instanceof TouchEvent
+      ? e.changedTouches[0].screenX
+      : e instanceof MouseEvent
+      ? e.screenX
+      : 0) - tx;
+  const n = data.width * data.height;
+  if (dx < -100) {
+    console.log("left", dx);
+    app.setData({ selected: (((data.selected - 1) % n) + n) % n });
+  } else if (dx > 100) {
+    console.log("right", dx);
+    app.setData({ selected: (((data.selected + 1) % n) + n) % n });
+  } else if (e.timeStamp - tt < 500) {
+    console.log("tap", dx);
+    if (data.selected >= 0) {
+      const word = document.querySelector(".selected figcaption").textContent;
+      const msg = new SpeechSynthesisUtterance(word);
+      speechSynthesis.speak(msg);
+    } else {
+      const target = /** @type {HTMLElement} */ (e.target);
+      const symbol = target.closest(".symbol");
+      if (symbol) {
+        const word = symbol.textContent;
+        const msg = new SpeechSynthesisUtterance(word);
+        speechSynthesis.speak(msg);
+      }
+    }
+  } else {
+    console.log("not");
+  }
+}
+
+document.addEventListener("render", (e) => {
   if (e.target instanceof HTMLElement && e.target.matches(".page")) {
     console.log("render", e);
     const symbols = document.querySelectorAll("div.symbol");
---
+++ twoswitch.js
@@ -0,0 +1,191 @@
+function highlightRow(r) {
+  console.log("hi", r);
+}
+
+function previewCell(r, c) {
+  console.log("pre", r, c);
+}
+
+function play(r, c) {
+  console.log("play", r, c);
+}
+
+let state = {
+  row: 0,
+  col: 0,
+  level: 1,
+  reps: 0,
+  nrows: 3,
+  ncols: 4,
+  maxReps: 2
+};
+
+/** determine the highlight
+ * @param {number} r
+ * @param {number} c
+ * @param {State} s */
+function highlight(r, c, s) {
+  let result =
+    (s.level == 1 && r == s.row && "row") ||
+    (s.level == 2 && r == s.row && c == s.col && "cell") ||
+    "";
+  return result;
+}
+
+/** @typedef {typeof state} State */
+
+/** @callback conditionCallback
+ * @param {State} s
+ * @returns {boolean} */
+
+/** @callback updateCallback
+ * @param {State} s
+ * @returns {Object}
+ */
+
+/** @callback actionCallback
+ * @param {State} s
+ */
+
+/** @typedef {Object} Rule
+ * @property {number} input
+ * @property {conditionCallback} condition
+ * @property {updateCallback} update
+ * @property {actionCallback} action
+ */
+
+/** @type {Rule[]} rules */
+let rules = [
+  {
+    input: 0,
+    condition: s => s.level == 1 && s.row < s.nrows,
+    update: s => ({ row: s.row + 1 }),
+    action: () => {}
+  },
+  {
+    input: 0,
+    condition: s => s.level == 1 && s.row == s.nrows,
+    update: () => ({ row: 1 }),
+    action: () => {}
+  },
+  {
+    input: 1,
+    condition: s => s.level == 1,
+    update: () => ({ level: 2, col: 1, reps: 1 }),
+    action: () => {}
+  },
+  {
+    input: 0,
+    condition: s => s.level == 2 && s.col < s.ncols,
+    update: s => ({ col: s.col + 1 }),
+    action: () => {}
+  },
+  {
+    input: 0,
+    condition: s => s.level == 2 && s.col == s.ncols && s.reps < s.maxReps,
+    update: s => ({ col: 1, reps: s.reps + 1 }),
+    action: () => {}
+  },
+  {
+    input: 0,
+    condition: s => s.level == 2 && s.col == s.ncols && s.reps == s.maxReps,
+    update: () => ({ level: 1 }),
+    action: () => {}
+  },
+  {
+    input: 1,
+    condition: s => s.level == 2,
+    update: () => ({ level: 1, row: 1 }),
+    action: s => {
+      play(s.row, s.col);
+    }
+  }
+];
+
+import Reef from "./reef.js";
+
+Reef.debug(true);
+
+/** tag html template strings so prettier will format them and vim will highlight them
+ * @returns string
+ * @param {TemplateStringsArray} strings
+ * @param {any[]} values
+ */
+function html(strings, ...values) {
+  let str = "";
+  strings.forEach((s, i) => {
+    str += s + (i < values.length ? values[i] : "");
+  });
+  return str;
+}
+
+/** @typedef {typeof state} Props */
+
+const app = new Reef("#app", {
+  data: state,
+  /** @param {Props} props */
+  template: props => {
+    // display the state
+    let h = "";
+    for (const state in props) {
+      h += html`
+        <li>${state}: ${props[state]}</li>
+      `;
+    }
+    h = html`
+      <ul>
+        ${h}
+      </ul>
+    `;
+    // draw the grid
+    let rows = "";
+    for (let r = 1; r <= props.nrows; r++) {
+      let columns = "";
+      for (let c = 1; c <= props.ncols; c++) {
+        let cls = highlight(r, c, props);
+        columns += html`
+          <td class="${cls}">${r},${c}</td>
+        `;
+      }
+      rows += html`
+        <tr>
+          ${columns}
+        </tr>
+      `;
+    }
+    return html`
+      ${h}
+      <hr />
+      <table border="1">
+        <tbody>
+          ${rows}
+        </tbody>
+      </table>
+    `;
+  }
+});
+
+window.addEventListener("load", () => app.render());
+
+document.addEventListener("keydown", e => {
+  console.log("kd", e);
+  const data = app.getData();
+  let input = null;
+  if (["ArrowRight", "Space", "Tab"].includes(e.code)) {
+    e.preventDefault();
+    input = 0;
+  } else if (["Enter", "ArrowLeft"].includes(e.code)) {
+    input = 1;
+  }
+  if (input !== null) {
+    for (const rule of rules) {
+      if (rule.input == input && rule.condition(data)) {
+        const update = rule.update(data);
+        rule.action(data);
+        app.setData(update);
+        console.log("update", update);
+        break;
+      }
+    }
+  }
+});
